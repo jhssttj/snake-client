89e2033a695fbd57571377ea614e549ff191cd8d
Kevin Lee <jhssttj@hotmail.com>
2022-09-28 15:02:00 -0600

    Modified to manually move the snake

--- input.js
+++ input.js
@@ -1,12 +1,27 @@
+// Stores the active TCP connection object.
+let connection;
+
 //Taking user inputs
 const handleUserInput = function (key) {
+  if (key === 'w') {
+    connection.write("Move: up")
+  }
+  if (key === 'a') {
+    connection.write("Move: left")
+  }
+  if (key === 's') {
+    connection.write("Move: down")
+  }
+  if (key === 'd') {
+    connection.write("Move: right")
+  }
   if (key === '\u0003') {
     process.exit();
   }
 };
 
-// setup interface to handle user input from stdin
-const setupInput = function () {
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
--- play.js
+++ play.js
@@ -1,11 +1,9 @@
-
 const {connect} = require("./client");
 const {setupInput} = require("./input");
 // establishes a connection with the game server
 
-
+const conn = connect();
 
 console.log("Connecting ...");
-connect();
 
-setupInput();
+setupInput(conn);
